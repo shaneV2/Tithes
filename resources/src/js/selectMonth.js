979d870aac34030ae09a9599377569029cd69bde
shnae <shane.prac@gmail.com>
2025-04-15 20:14:09 +0800

    create design for month options

---
+++ resources/src/js/selectMonth.js
@@ -0,0 +1,8 @@
+function selectMonth(){
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.getElementById("months-options").childNodes;
+    console.log(buttons)
+})
