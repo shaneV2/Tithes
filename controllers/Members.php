91342e613a96fb1af25796aeeba37d09138245c7
shnae <shane.prac@gmail.com>
2025-06-18 11:13:57 +0800

    implement add member logic

--- controllers/Members.php
+++ controllers/Members.php
@@ -1,5 +1,37 @@
 <?php 
     class Members extends Database {
+        public function addMember($id){
+            $connection = $this->getConnection();
+
+            $stmt = $connection->prepare("select user_code as code from users where user_code=? or username=?");
+            $stmt->bind_param("ss", $id, $id);
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+            if (mysqli_num_rows($result)> 0){
+                try {
+                    $code = mysqli_fetch_assoc($result)['code'];
+                    $insert_stmt = $connection->prepare("insert into members(member_code) values(?)");
+                    $insert_stmt->bind_param("s", $code);
+                    $insert_stmt->execute();
+                } catch (\Throwable $th) {
+                    session_start();
+                    $_SESSION['user_exist_error'] = true;
+                    $_SESSION['identification'] = $id;
+                } finally {
+                    if (isset($insert_stmt) && $insert_stmt instanceof mysqli_stmt){
+                        $insert_stmt->close();
+                    }
+                }
+            } else {
+                session_start();
+                $_SESSION['id_not_found_error'] = true;
+            }
+
+            $connection->close();
+            $stmt->close();
+        }
+
         public function getMembersBasedOnDate($date_id){
             $conn = $this->getConnection();
             $stmt = $conn->prepare('select * from user_offers where date_id = ? order by id desc');
